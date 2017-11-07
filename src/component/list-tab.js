54a06299f95cad899e8a2af617ed821bb2215368
tao.liu <tao.liu@rea-group.com>
2017-11-07 12:42:46 +0800

    重构list加载

--- src/component/list-tab.js
+++ src/component/list-tab.js
@@ -1,50 +1,55 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, Link } from "react-router";
-// import { bindActionCreators } from "redux";
-// import { connect } from "react-redux";
-// import { addOne } from "../actions";
-// import { authenticateUser } from "../actions";
-// import { store } from "../index";
+import {NO_ITEM, ITEMS_PER_PAGE } from '../env_variable';
+
 
 class ListTab extends Component {
+
   constructor(props) {
     super(props);
-    // this.items = this.props.items; // 记录 全用this的方式定义仅这个class本身给自己用到的变量，使用const或者let可能会出问题
-    this.nextScrollIndex = 5;
-    this.state = {
-      items: this.props.items.slice(0,6),
-    }
+    this.pages = Math.ceil(this.props.items.length / ITEMS_PER_PAGE);
+    this.currentPageStartNum = 0;
+    this.currentPageEndNum = ITEMS_PER_PAGE;
     this.onScroll = this.onScroll.bind(this);
+    this.state = {
+      items: this.props.items.slice(0, ITEMS_PER_PAGE),
+      pages: this.pages,
+      isEnd: false
+    }
   }
 
-  async componentWillReceiveProps(nextProps) {
-    this.nextScrollIndex = 5;    
-    await this.setState({items: nextProps.items.slice(0,6)}) //异步的
-    // console.log("new props", nextProps.items);
-    // console.log("new state", this.state.items);
+  componentWillReceiveProps(nextProps) {
+    this.pages = Math.ceil(nextProps.items.length / ITEMS_PER_PAGE);
+    this.currentPageStartNum = 0;
+    this.currentPageEndNum = ITEMS_PER_PAGE;
+    this.setState({
+      items: nextProps.items.slice(0, ITEMS_PER_PAGE),
+      pages: this.pages,
+      isEnd: false
+    })
   }
 
-  async onScroll() {
-      if(this.props.items.length<7){
-        console.log("到底啦~~", this.nextScrollIndex);
-        console.log("items.length<7");
-        return;
-      } 
+  onScroll() {
+    //当从有scroll变为无scroll的时候会触发onScroll，且判定为滑到底了。所以多页变成一页时会onScroll
+    if (this.props.items.length < ITEMS_PER_PAGE) {
+      return null;
+    } //确保多到一时不触发onScroll内容执行
     let listEl = ReactDOM.findDOMNode(this.ulList);
-    if(listEl.scrollTop + listEl.clientHeight === listEl.scrollHeight) {
-      console.log("到底啦~~2");
-      let i = 0;
-      for(i; i<5; i++) {
-        ++this.nextScrollIndex;
-        if(this.props.items[this.nextScrollIndex]) {
-          let nextItem = this.props.items[this.nextScrollIndex];
-          await this.setState({items: [...this.state.items, nextItem]});
-        } else {
-          --this.nextScrollIndex;
-          console.log("到底啦啦啦啦啦啦啦啦啦",this.nextScrollIndex);
-          break;
-        }
+    if (listEl.scrollTop + listEl.clientHeight === listEl.scrollHeight) {
+      --this.pages;
+      if (this.pages > 0) {
+        this.currentPageStartNum += ITEMS_PER_PAGE;
+        this.currentPageEndNum += ITEMS_PER_PAGE;
+        this.setState({
+          items: [
+            ...this.state.items,
+            ...this.props.items.slice(this.currentPageStartNum, this.currentPageEndNum)
+          ]
+        })
+      } else if (this.pages === 0) {
+        this.setState({isEnd: true});
+      } else {
+        return null;
       }
     }
   }
@@ -52,18 +57,19 @@ class ListTab extends Component {
   render() {
     return (
       <div>
-          <div>TabB</div>
-        <ul ref={(ul) => {this.ulList = ul; }} className="item-listing" onScroll={this.onScroll}>
-          {this.state.items.length === 0        //这里如果换成this.items就会出现延迟rerender
-            ? "hahhaahahahahahh"
-            : this.state.items.map((item, index) => {  // 目前认为是rerender不会再执行constructor，所以this.items不变
-                return (
-                  <div key={index}>
-                    <li><span>name: {item.name}</span>
-                    <span>&nbsp;&nbsp;&nbsp;&nbsp;phone: {item.phone}</span></li>
-                  </div>
-                );
-              })}
+        <div>TabB</div>
+        <ul ref={(ul) => {
+          this.ulList = ul;
+        }} className="item-listing" onScroll={this.onScroll}>
+          {this.state.items.length === 0
+            ? NO_ITEM
+            : this.state.items.map((item, index) => {
+              return (
+                <li key={index}><span>name: {item.name}</span>
+                  <span>&nbsp;&nbsp;&nbsp;&nbsp;phone: {item.phone}</span></li>
+              );
+            })}
+          {this.state.isEnd ? <li><span>到底啦</span></li> : null}
         </ul>
       </div>
     );
--- src/env_variable.js
+++ src/env_variable.js
@@ -1,3 +1,5 @@
 export const LOGIN_SUCCESS = "已登录";
 export const LOGIN_FAIL = "用户名或密码错误";
 export const HAVE_NOT_LOGIN = "请登陆";
+export const NO_ITEM = "当前没有数据";
+export const ITEMS_PER_PAGE = 5;
