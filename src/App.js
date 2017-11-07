294b169b890cdd2a180c8b6b44439cdc93e0b867
tao.liu <tao.liu@rea-group.com>
2017-11-08 00:14:32 +0800

    重构代码-验证登陆状态

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Menu } from "./component/menu";
-import { LOGIN_SUCCESS } from "./env_variable";
+import { loginAuthenticate } from "./component/login";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.loginAuthenticate = this.loginAuthenticate.bind(this);
-  }
-
-  loginAuthenticate(routeText) {
-    if (this.props.authenticateReducer.loginState !== LOGIN_SUCCESS) {
-      return "/login";
-    } else {
-      return routeText;
-    }
-  }
 
   render() {
     return (
@@ -28,15 +16,13 @@ class App extends Component {
           </div>
           <Menu
             menuItem={this.props.children}
-            loginAuthenticate={this.loginAuthenticate}
+            loginAuthenticate={(routeText) => loginAuthenticate() ? routeText : "/login"}
           />
         </div>
       </div>
     );
   }
 }
-
-
 
 const mapStateToProps = state => state;
 
